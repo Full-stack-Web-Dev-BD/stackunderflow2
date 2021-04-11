d8bfe064558f1818eb285303ee052d499def1c38
Alamin <alaminprogramerr@gmail.com>
2021-04-11 15:57:29 +0600

    connected local db

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 
 dotenv.config();
 var mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://user:user@mern.a77ou.mongodb.net/stackdb?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://localhost/stackunderflow', { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
     if (err) {
         return console.log('Database connection  failed')
     }
